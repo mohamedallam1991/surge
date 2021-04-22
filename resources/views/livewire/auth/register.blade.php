5a73169d143d177107b31a4779cf1100e7af455a
Mohamed Allam <meda3gm@gmail.com>
2021-04-23 00:19:12 +0100

    adding the form frontend

--- resources/views/livewire/auth/register.blade.php
+++ resources/views/livewire/auth/register.blade.php
@@ -4,7 +4,29 @@
     <div>
 
 
-        hello registeration
+
+        <form action="/register" method="POST">
+
+        <div>
+            <label for="email">Email</label>
+            <input type="text" id="email" name="email">
+        </div>
+
+        <div>
+            <label for="password">Password </label>
+            <input type="text" id="password" name="password">
+        </div>
+
+        <div>
+            <label for="passwordConfirmation">Password Confirmation</label>
+            <input type="text" id="passwordConfirmation" name="passwordConfirmation">
+        </div>
+
+        <div>
+            <input type="submit" value="Register">
+        </div>
+
+        </form>
 
 
 
